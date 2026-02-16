79197e4798f31600323b26b8149df4d9d0243f25
Wisaroot Lertthaweedech <66859294+wislertt@users.noreply.github.com>
2026-02-16 18:05:55 +0700

    feat: add bakefile.py (#30)

---
+++ bakefile.py
@@ -0,0 +1,36 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "bakefile[lib]>=0.0.26",
+# ]
+#
+# # Debug with local bakefile in editable mode
+# # [tool.uv.sources]
+# # bakefile = { path = "../bakefile", editable = true }
+# ///
+
+from bake import command
+from bakelib import RustSpace
+
+
+class MyBakebook(RustSpace):
+    def test(self) -> None:
+        env: dict[str, str] = {}
+        env["RUST_LOG"] = "cargo_tarpaulin=off"
+        self.ctx.run(
+            "cargo tarpaulin --out Xml --out Html --out Lcov "
+            "--output-dir coverage --exclude-files 'src/main.rs' "
+            "-- --quiet",
+            env=env,
+        )
+
+    @command()
+    def run(self) -> None:
+        self.ctx.run("cargo run")
+
+    @command()
+    def open_coverage(self):
+        self.ctx.run("open coverage/tarpaulin-report.html")
+
+
+bakebook = MyBakebook()
